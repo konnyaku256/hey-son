66c31123a8fad6f2450febfda68cdd702ffecd31
konnyaku256 <29503528+konnyaku256@users.noreply.github.com>
2020-01-13 18:41:27 +0900

    :bug Fix CORS

--- frontend/src/components/HeySonButton.vue
+++ frontend/src/components/HeySonButton.vue
@@ -5,7 +5,11 @@
 </template>
 
 <script>
-import axios from "@/assets/axios.js";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://192.168.0.16:3000";
+axios.defaults.headers.post["Content-Type"] = "application/json;charset=utf-8";
+axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
 
 export default {
   name: "HeySonButton",
