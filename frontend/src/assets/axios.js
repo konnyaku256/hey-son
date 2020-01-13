46c58169e6a179b9ba75303c1b5eef90fee4dff4
konnyaku256 <29503528+konnyaku256@users.noreply.github.com>
2020-01-13 18:37:35 +0900

    :bug: Fix settings for export

--- frontend/src/assets/axios.js
+++ frontend/src/assets/axios.js
@@ -3,3 +3,5 @@ import axios from "axios";
 axios.defaults.baseURL = "http://192.168.0.16:3000";
 axios.defaults.headers.post["Content-Type"] = "application/json;charset=utf-8";
 axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
+
+export default axios;
