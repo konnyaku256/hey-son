66e9fabe8432c835358702af67c2fb4299609115
konnyaku256 <29503528+konnyaku256@users.noreply.github.com>
2020-01-13 19:22:41 +0900

    :bug: Fix settings for axios

--- frontend/src/components/HeySonButton.vue
+++ frontend/src/components/HeySonButton.vue
@@ -5,12 +5,7 @@
 </template>
 
 <script>
-import axios from "axios";
-
-axios.defaults.baseURL = "http://192.168.0.16:3000";
-axios.defaults.headers.post["Content-Type"] = "application/json;charset=utf-8";
-axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
-
+import axios from "@/assets/axios.js";
 export default {
   name: "HeySonButton",
   props: {
